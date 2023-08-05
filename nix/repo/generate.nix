ebe5cda2600001bfb971b59bdf4ccad71e7aa132
Aidan Chelig <aidan.chelig@gmail.com>
2023-08-05 13:35:56 -0500

    add templating?

--- nix/repo/generate.nix
+++ nix/repo/generate.nix
@@ -6,28 +6,12 @@
   inherit (inputs) nixpkgs std;
 
   l = nixpkgs.lib // builtins;
-
-  package = cell.packages.default;
 in {
-  generatePage = std.lib.ops.mkOperable {
-    inherit package;
-
-    meta.mainProgram = "operable-leptos_portfolio";
-
-    runtimeEnv = {
-      LEPTOS_OUTPUT_NAME = "leptos_portfolio";
-      LEPTOS_SITE_ROOT = "${package}/usr/share/site";
-      LEPTOS_SITE_PKG_DIR = "pkg";
-      LEPTOS_SITE_ADDR = "0.0.0.0:3000";
-      LEPTOS_RELOAD_PORT = "3001";
-    };
-
-    debugInputs = with nixpkgs; [
-      nettools
-    ];
-
-    runtimeScript = ''
-      ${package}/bin/leptos_portfolio
+  generate = std.lib.ops.writeScript {
+    runtimeInputs = [];
+    name = "generate";
+    text = ''
+      echo "INITsss"
     '';
   };
 }
