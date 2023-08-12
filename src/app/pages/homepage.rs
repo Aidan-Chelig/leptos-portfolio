6342934148f1cb44972d8ef7442eaac57958c8ff
aidan chelig <aidan.chelig@gmail.com>
2023-08-11 22:07:47 -0500

    add test var

--- src/app/pages/homepage.rs
+++ src/app/pages/homepage.rs
@@ -3,7 +3,6 @@ use leptos::*;
 #[component]
 pub fn HomePage(cx: Scope) -> impl IntoView {
 	// creates a reactive value to update the button
-					let swaggy = 10;
 	view! { cx,
 		<h1>"home"</h1>
 		<div class="">
