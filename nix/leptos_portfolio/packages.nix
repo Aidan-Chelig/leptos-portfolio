bcd6cf779388d7a5c7182eb17851576ea8d2afd5
aidan chelig <aidan.chelig@gmail.com>
2023-08-13 19:05:07 -0500

    almost there?

--- nix/leptos_portfolio/packages.nix
+++ nix/leptos_portfolio/packages.nix
@@ -4,6 +4,8 @@
 }: let
   inherit (inputs) std self cells nixpkgs;
 
+  l = nixpkgs.lib // builtins;
+
   leptosNativeBuildInputs = with nixpkgs; [
     cargo-leptos
     binaryen
@@ -13,21 +15,27 @@
   ];
 
   commonArgs = {
-    src = std.incl self [
-      "${self}/Cargo.lock"
-      "${self}/Cargo.toml"
-      "${self}/src"
-      "${self}/assets"
-      "${self}/style"
-    ];
+    src = lib.cleanSourceWith {
+      src = ./.; # The original, unfiltered source
+      filter = path: type:
+        (l.hasSuffix "\.html" path)
+        || (l.hasSuffix "\.scss" path)
+        ||
+        # Example of a folder for images, icons, etc
+        (l.hasInfix "/assets/" path)
+        ||
+        # Default filter from crane (allow .rs files)
+        (crane.filterCargoSources path type);
+    };
   };
 
+  # TODO  get this working!!!!
   cargoArtifacts = crane.buildDepsOnly (commonArgs
     // {
       cargoExtraArgs = "--profile release";
     });
 
-  leptos_portfolio = crane.buildPackage (commonArgs
+  leptos_portfolio_ssr = crane.buildPackage (commonArgs
     // rec {
       nativeBuildInputs = leptosNativeBuildInputs;
       cargoBuildCommand = "cargo leptos --log wasm --log server build";
@@ -45,9 +53,30 @@
       inherit cargoArtifacts;
     });
 
+  wasmArgs =
+    commonArgs
+    // {
+      pname = "leptos_portfolio_csr";
+      cargoExtraArgs = "--features csr";
+      CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+    };
+
+  cargoArtifactsWasm = crane.buildDepsOnly (wasmArgs
+    // {
+      doCheck = false;
+    });
+
+  leptos_portfolio_csr = crane.buildTrunkPackage (wasmArgs
+    // {
+      pname = "leptos_portfolio_csr";
+      cargoArtifacts = cargoArtifactsWasm;
+      trunkIndexPath = "index.html";
+    });
+
   crane = inputs.crane.lib.overrideToolchain cells.repo.rust.toolchain;
 in {
   #sane default for a binary package
 
-  default = leptos_portfolio;
+  default = leptos_portfolio_ssr;
+  leptos_portfolio_csr = leptos_portfolio_csr;
 }
